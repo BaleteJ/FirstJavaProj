40b90278d775969323aed26c53b4ad8f0997992f
BaleteJ <balete.jason27@gmail.com>
2021-03-25 21:24:58 +0800

    Changed main file and removed other files

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args){
+        //Primitive
+        int wholeNumbers = 122;
+        long largeDecimalNumbers = -12;
+        short smallDecimalNumbers = 12;
+        byte byteData = 120;
+        float floatData = 12.3f;
+        double doubleData = 45.5672;
+        boolean booleanData = true;
+        char charData = 'd';
+
+        //Print all primitive types, output and run
+        System.out.println("Int: " + wholeNumbers + " Long: " + largeDecimalNumbers + " Short: " + smallDecimalNumbers +
+                " Byte: " + byteData + " Float: " + floatData +" Double: " + doubleData +
+                " Boolean: " + booleanData + " Char: " + charData);
+
+
+
+        //Non Primitive
+
+
+        //If statement
+        if(booleanData)
+        {
+            System.out.println("Statement true"+largeDecimalNumbers);
+        }
+    }
+}
